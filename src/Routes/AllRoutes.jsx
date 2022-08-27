01c18353f8da4f1b362bf1b98224097507d29eb2
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-08-27 23:41:40 +0530

    day-5

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import HowItWorks from "./Pages/HowItWorks";
+import Signinpage from "./Pages/Signin";
+import SignUp from "./Pages/Signup";
 import UseCases from "./Pages/UseCases";
 
 export default function AllRoutes() {
@@ -11,8 +13,8 @@ export default function AllRoutes() {
       <Route path="/use-cases" element={<UseCases />} />
       <Route path="/pricing" element={<h1>Pricing Page</h1>} />
       <Route path="/support" element={<h1>Support Page</h1>} />
-      <Route path="/signin" element={<h1>Sing in Page</h1>} />
-      <Route path="/signup" element={<h1>Sing up Page</h1>} />
+      <Route path="/signin" element={<Signinpage />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
